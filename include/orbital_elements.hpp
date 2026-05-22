1194e95dbe780e45ab3ac64769e23b5f84244651
Ludovic Andrieux <149336350+ludodo23@users.noreply.github.com>
2026-05-22 12:58:33 +0200

    Update orbital_elements.hpp

--- include/orbital_elements.hpp
+++ include/orbital_elements.hpp
@@ -1,8 +1,49 @@
 #pragma once
 
+
+/*  
+ * License: CeCILL-C  
+ *  
+ * Copyright (c) 2026 Ludovic Andrieux  
+ * contributor(s): Ludovic Andrieux (2026)  
+ *  
+ * ludovic.andrieux23@gmail.com  
+ *  
+ * This software is a header-only C++ library for orbital elements conversions.
+ *  
+ * This software is governed by the CeCILL-C license under French law and  
+ * abiding by the rules of distribution of free software. You can use,  
+ * modify and/or redistribute the software under the terms of the CeCILL-C  
+ * license as circulated by CEA, CNRS and INRIA at the following URL:  
+ * https://www.cecill.info  
+ *  
+ * As a counterpart to the access to the source code and rights to copy,  
+ * modify and redistribute granted by the license, users are provided only  
+ * with a limited warranty and the software's author, the holder of the  
+ * economic rights, and the successive licensors have only limited  
+ * liability.  
+ *  
+ * In this respect, the user's attention is drawn to the risks associated  
+ * with loading, using, modifying and/or developing or reproducing the  
+ * software by the user in light of its specific status of free software,  
+ * that may mean that it is complicated to manipulate, and that also  
+ * therefore means that it is reserved for developers and experienced  
+ * professionals having in-depth computer knowledge.  
+ *  
+ * The fact that you are presently reading this means that you have had  
+ * knowledge of the CeCILL-C license and that you accept its terms.  
+ */  
+
+#include <algorithm>
+#include <array>
+#include <cmath>
+#include <stdexcept>
+#include <string>
+
+#define ORBIT_VERSION "0.1.0"
+
 /**
- * @file orbital_elements.hpp
- * @brief Header-only C++ library for orbital elements conversions.
+ * @brief orbital elements conversions.
  *
  * Supported element sets:
  *   - Classical Keplerian       (COE)  : {a, e, i, RAAN, omega, nu}
@@ -45,16 +86,7 @@
  *   auto poi   = coe_to_poincare(coe, MU_EARTH);
  *   auto coe2  = mee_to_coe(mee);
  * @endcode
- */
-
-#include <algorithm>
-#include <array>
-#include <cmath>
-#include <stdexcept>
-#include <string>
-
-#define ORBIT_VERSION "0.1.0"
- 
+ */ 
 namespace orb {
 
 // ============================================================================
